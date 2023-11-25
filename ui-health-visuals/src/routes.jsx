e622e150ba4f2a409a3ac73340185e2c9daf0b9c
Animesh Nema <animeshnema@Animeshs-MacBook-Air.local>
2023-11-25 00:39:09 -0600

    Added basic structure

---
+++ ui-health-visuals/src/routes.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+// import Home from '../pages/Home';
+// import About from '../pages/About';
+// import Contact from '../pages/Contact';
+// import NotFound from '../pages/NotFound';
+// import PrivateRoute from './PrivateRoute';
+
+const Routes = () => {
+  return (
+    // <Switch>
+    //   Public Routes
+    //   <Route exact path="/" component={Home} />
+    //   <Route path="/about" component={About} />
+    //   <Route path="/contact" component={Contact} />
+
+    //   {/* Private Route - Requires authentication */}
+    //   <PrivateRoute path="/dashboard" component={Dashboard} />
+
+    //   {/* 404 Not Found Route */}
+    //   <Route component={NotFound} />
+      
+    // </Switch>
+    <div></div>
+  );
+};
+
+export default Routes;
